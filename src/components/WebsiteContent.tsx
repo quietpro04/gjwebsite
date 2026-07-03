c4eb12c6eb8bfb3e2ffb7be0a6a828d81911052b
quietpro04 <greg.joblove@gmail.com>
2026-07-03 11:02:28 -0400

    Fixed second instance of short reel

--- src/components/WebsiteContent.tsx
+++ src/components/WebsiteContent.tsx
@@ -249,7 +249,7 @@ const WebsiteContent: React.FC<WebsiteContentProps> = ({ isVisible }) => {
                 controls
               >
                 <source
-                  src="https://media.gregjoblove.com/reels/2026_Short_General_v1.webm"
+                  src="https://media.gregjoblove.com/reels/2026_Short_General_v2.webm"
                   type="video/webm"
                 />
                 Your browser does not support the video tag.
